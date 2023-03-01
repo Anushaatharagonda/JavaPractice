63c9c26d6f03d783d7392004913977c4fc75d563
Anusha <jatharagondaanushaj@gmail.com>
2023-03-01 17:56:17 +0530

    ArrayList

---
+++ ArrayList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Arraylist
+{
+public static void main(String args[])
+{
+ArrayList l=new ArrayList();
+l.add("a");
+l.add(10);
+l.add('$');
+l.add(true);
+l.add(null);
+System.out.println(l);
+l.add(10);
+System.out.println(l.get(2));
+}
+}
