48ee2b7d15688a94685481968e727d1e2c4e78b6
Anusha <jatharagondaanushaj@gmail.com>
2023-03-01 18:24:36 +0530

    LinkedList

---
+++ linkedlist.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class linkedlist
+{
+public static void main(String args[])
+{
+LinkedList a=new LinkedList();
+a.add(13);
+a.add("anu");
+a.add('a');
+a.add(true);
+a.addFirst("anusha");
+a.addLast("Ajay");
+System.out.println(a);
+System.out.println(a.size());
+a.remove(3);
+System.out.println(a);
+System.out.println(a.get(1));
+a.set(2,'s');
+System.out.println(a);
+}
+}
+
